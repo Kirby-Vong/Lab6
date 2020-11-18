9a320ec15b57790ff1b6e3fc79c6d62676e23966
Kirby-Vong <kirbyvong@gmail.com>
2020-11-17 17:12:18 -0800

    Change Unit Test

--- unit_test.cpp
+++ unit_test.cpp
@@ -1,12 +1,5 @@
 #include "gtest/gtest.h"
 
-#include "op_test.hpp"
-#include "rand_test.hpp"
-#include "div_test.hpp"
-#include "sub_test.hpp"
-#include "pow_test.hpp"
-#include "add_test.hpp"
-#include "mult_test.hpp"
 #include "op.hpp"
 #include "VectorContainer_test.hpp"
 
